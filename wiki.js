fd8a46d0f0f25ea92d867b5fddddf70d95d76b52
Christopher Li <chrisli41@gmail.com>
2016-10-12 15:08:41 -0700

    Initial Commit

---
+++ wiki.js
@@ -0,0 +1,45 @@
+/**
+ * Created by christopherli on 9/14/16.
+ */
+$(document).ready(function(){
+
+    var shift = 1;
+
+    $("#button").on('click', function(){
+
+        var searchTxt = $('#search').val();
+        var apiLink = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
+        var apiRest = "&limit=20&format=json&callback=?";
+
+        var apiCall = apiLink + searchTxt + apiRest;
+
+        if(shift && searchTxt) {
+            $("#page-wrapper").animate({top: '-=200px'}, 2000);
+            shift = 0;
+        }
+        
+        $.getJSON(apiCall, function(list){
+
+            $('#entries').html('');
+            $('#entries').hide();
+
+            for(var i = 0; i < list[1].length; i++) {
+
+                var title = list[1][i];
+                var description = list[2][i];
+                var link = list[3][i];
+
+                var item = '<a class="item-link" href="' + link + '"><div class="item hvr-glow"><h3>' + title + '</h3><p>' + description + '</p></div></a>';
+
+                $('#entries').append(item);
+                
+
+            }
+
+            $('#entries').fadeIn('slow');
+
+        });
+        
+    });
+    
+});
